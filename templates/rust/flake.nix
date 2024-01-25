a43ffe6b1e71be83dba5907265cdbcd051de9210
sachnr <sachinrawat544@gmail.com>
2024-01-25 14:41:58 +0530

    stable rust in template

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -15,23 +15,26 @@
           (import rust-overlay)
         ];
       };
-      toolchain = pkgs.rust-bin.selectLatestNightlyWith (toolchain:
-        toolchain.default.override {
-          extensions = ["rust-src" "rust-analyzer"];
-          targets = ["wasm32-unknown-unknown"];
-        });
+      toolchain = pkgs.rust-bin.stable.latest.default.override {
+        extensions = ["rust-src" "rust-analyzer"];
+        targets = [];
+      };
+      # toolchain = pkgs.rust-bin.selectLatestNightlyWith (toolchain:
+      #   toolchain.default.override {
+      #     extensions = ["rust-src" "rust-analyzer"];
+      #     targets = ["wasm32-unknown-unknown"];
+      #   });
     in {
       devShells.default = pkgs.mkShell {
         name = "rust-app";
         buildInputs = with pkgs; [
-          openssl
-          pkg-config
           toolchain
+          pkg-config
         ];
         RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.openssl];
+        # LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.openssl];
         shellHook = ''
-          rustup override set nightly
+          # rustup override set nightly
         '';
       };
     });

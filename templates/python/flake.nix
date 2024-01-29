ea86a35874742b3cdc1b2b6dd2c5100b8685c0e0
sachnr <sachinrawat544@gmail.com>
2024-01-29 18:43:58 +0530

    py template

---
+++ templates/python/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Python Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    mach-nix.url = "github:DavHau/mach-nix";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = inputs @ {self, ...}:
+    inputs.flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+      };
+      py-deps = inputs.mach-nix.lib."${system}".mkPython {
+        # requirements = builtins.readFile ./requirements.txt;
+        requirements = ''
+          numpy
+          python-lsp-server
+        '';
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "pyenv";
+        buildInputs = with pkgs; [
+          # pkg-config
+          python311
+          py-deps
+        ];
+      };
+    });
+}
